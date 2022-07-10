5bf115e3354d68f8510bc502a1de2b87602d736b
cqw <sophia.cq.wang@gmail.com>
2022-07-09 21:34:12 -0400

    step-25

--- lib/views/verify_email_view.dart
+++ lib/views/verify_email_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'package:mynotes/constants/routes.dart';
-import 'package:mynotes/services/auth/auth_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
 
 class VerifyEmailView extends StatefulWidget {
   const VerifyEmailView({Key? key}) : super(key: key);
@@ -22,18 +22,14 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
           const Text("A verification email has already been sent to you."),
           const Text("If you don't see the email: "),
           TextButton(
-            onPressed: () async {
-              await AuthService.firebase().sendEmailVerifiction();
+            onPressed: () {
+              context.read<AuthBloc>().add(const AuthEventSendEmailVerification());
             },
             child: const Text("Send email verification again"),
           ),
           TextButton(
             onPressed: () async {
-              await AuthService.firebase().logOut();
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                registerRoute,
-                (route) => false,
-              );
+              context.read<AuthBloc>().add(const AuthEventLogOut());
             },
             child: const Text("Or restart"),
           )
